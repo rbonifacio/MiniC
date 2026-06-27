19ad0117183e5ce2c6e71fc66dea06c221d22f6e
AlvaroNegromonte <acn3@cin.ufpe.br>
2026-06-27 17:44:24 -0300

    test: add TAC tests for lambda generation

--- tests/tac_gen.rs
+++ tests/tac_gen.rs
@@ -1,43 +1,56 @@
 //! Integration tests for the MiniC TAC code generator.
 
-use mini_c::ir::ast::{CheckedExpr, CheckedStmt, ExprD, Expr, Literal, Statement, StatementD, Type};
+use mini_c::codegen::tac_code_gen::{translate_statement, Environment};
+use mini_c::ir::ast::{
+    CheckedExpr, CheckedStmt, Expr, ExprD, Literal, Statement, StatementD, Type,
+};
 use mini_c::ir::tac::{Address, Instruction, Operator};
-use mini_c::codegen::tac_code_gen::{Environment, translate_statement};
 
 // --- Helpers to build type-annotated AST nodes ---
 
 fn int_var(name: &str) -> CheckedExpr {
-    ExprD { exp: Expr::Ident(name.to_string()), ty: Type::Int }
+    ExprD {
+        exp: Expr::Ident(name.to_string()),
+        ty: Type::Int,
+    }
 }
 
 fn add(left: CheckedExpr, right: CheckedExpr) -> CheckedExpr {
-    ExprD { exp: Expr::Add(Box::new(left), Box::new(right)), ty: Type::Int }
+    ExprD {
+        exp: Expr::Add(Box::new(left), Box::new(right)),
+        ty: Type::Int,
+    }
 }
 
 fn lt(left: CheckedExpr, right: CheckedExpr) -> CheckedExpr {
-    ExprD { exp: Expr::Lt(Box::new(left), Box::new(right)), ty: Type::Bool }
+    ExprD {
+        exp: Expr::Lt(Box::new(left), Box::new(right)),
+        ty: Type::Bool,
+    }
 }
 
 fn mul(left: CheckedExpr, right: CheckedExpr) -> CheckedExpr {
-    ExprD { exp: Expr::Mul(Box::new(left), Box::new(right)), ty: Type::Int }
+    ExprD {
+        exp: Expr::Mul(Box::new(left), Box::new(right)),
+        ty: Type::Int,
+    }
 }
 
 fn int_lit(value: i64) -> CheckedExpr {
-    ExprD { exp: Expr::Literal(Literal::Int(value)), ty: Type::Int }
+    ExprD {
+        exp: Expr::Literal(Literal::Int(value)),
+        ty: Type::Int,
+    }
 }
 
 fn fn_int_int() -> Type {
     Type::Fun(vec![Type::Int], Box::new(Type::Int))
 }
 
-fn return_stmt(expr: CheckedExpr) -> CheckedStmt {
-    StatementD { stmt: Statement::Return(Some(Box::new(expr))), ty: Type::Int }
-}
-
 fn lambda_double() -> CheckedExpr {
     let param_name = "x".to_string();
     let body = StatementD {
-        stmt: Statement::Return(Some(Box::new(mul(int_var(&param_name), int_lit(2)))),),
+        stmt: Statement::Return(Some(Box::new(mul(int_var(&param_name), int_lit(2))))),
         ty: Type::Int,
     };
     ExprD {
@@ -51,13 +64,22 @@ fn lambda_double() -> CheckedExpr {
 }
 
 fn call_expr(callee: CheckedExpr, args: Vec<CheckedExpr>, ty: Type) -> CheckedExpr {
-    ExprD { exp: Expr::CallExpr { chmd: Box::new(callee), args }, ty }
+    ExprD {
+        exp: Expr::CallExpr {
+            chmd: Box::new(callee),
+            args,
+        },
+        ty,
+    }
 }
 
 fn assign(name: &str, value: CheckedExpr) -> CheckedStmt {
     StatementD {
         stmt: Statement::Assign {
-            target: Box::new(ExprD { exp: Expr::Ident(name.to_string()), ty: value.ty.clone() }),
+            target: Box::new(ExprD {
+                exp: Expr::Ident(name.to_string()),
+                ty: value.ty.clone(),
+            }),
             value: Box::new(value),
         },
         ty: Type::Unit,
@@ -78,7 +100,7 @@ fn assign(name: &str, value: CheckedExpr) -> CheckedStmt {
 fn test_if_else_with_relational_condition() {
     let stmt = StatementD {
         stmt: Statement::If {
-            cond:        Box::new(lt(int_var("x"), int_var("y"))),
+            cond: Box::new(lt(int_var("x"), int_var("y"))),
             then_branch: Box::new(assign("z", add(int_var("x"), int_var("y")))),
             else_branch: Some(Box::new(assign("z", int_var("x")))),
         },
@@ -88,22 +110,30 @@ fn test_if_else_with_relational_condition() {
     let mut env = Environment::new();
     let instructions = translate_statement(stmt, &mut env);
 
-    let x    = Address::Variable("x".to_string(), Type::Int);
-    let y    = Address::Variable("y".to_string(), Type::Int);
-    let z    = Address::Variable("z".to_string(), Type::Int);
+    let x = Address::Variable("x".to_string(), Type::Int);
+    let y = Address::Variable("y".to_string(), Type::Int);
+    let z = Address::Variable("z".to_string(), Type::Int);
     let temp = Address::Temporary("temp1".to_string(), Type::Int);
 
-    assert_eq!(instructions, vec![
-        Instruction::ConditionalJMPRelational(Operator::LT, x.clone(), y.clone(), "Label1:".to_string()),
-        Instruction::JMP("Label2:".to_string()),
-        Instruction::Label("Label1:".to_string()),
-        Instruction::BinaryAssignment(Operator::Add, temp.clone(), x.clone(), y.clone()),
-        Instruction::CopyAssignment(z.clone(), temp),
-        Instruction::JMP("Label3:".to_string()),
-        Instruction::Label("Label2:".to_string()),
-        Instruction::CopyAssignment(z, x),
-        Instruction::Label("Label3:".to_string()),
-    ]);
+    assert_eq!(
+        instructions,
+        vec![
+            Instruction::ConditionalJMPRelational(
+                Operator::LT,
+                x.clone(),
+                y.clone(),
+                "Label1:".to_string()
+            ),
+            Instruction::JMP("Label2:".to_string()),
+            Instruction::Label("Label1:".to_string()),
+            Instruction::BinaryAssignment(Operator::Add, temp.clone(), x.clone(), y.clone()),
+            Instruction::CopyAssignment(z.clone(), temp),
+            Instruction::JMP("Label3:".to_string()),
+            Instruction::Label("Label2:".to_string()),
+            Instruction::CopyAssignment(z, x),
+            Instruction::Label("Label3:".to_string()),
+        ]
+    );
 }
 
 #[test]
@@ -114,15 +144,28 @@ fn test_lambda_assignment_and_indirect_call() {
             seq: vec![
                 StatementD {
                     stmt: Statement::Assign {
-                        target: Box::new(ExprD { exp: Expr::Ident("double".to_string()), ty: double_ty.clone() }),
+                        target: Box::new(ExprD {
+                            exp: Expr::Ident("double".to_string()),
+                            ty: double_ty.clone(),
+                        }),
                         value: Box::new(lambda_double()),
                     },
                     ty: Type::Unit,
                 },
                 StatementD {
                     stmt: Statement::Assign {
-                        target: Box::new(ExprD { exp: Expr::Ident("result".to_string()), ty: Type::Int }),
-                        value: Box::new(call_expr(ExprD { exp: Expr::Ident("double".to_string()), ty: double_ty.clone() }, vec![int_lit(21)], Type::Int)),
+                        target: Box::new(ExprD {
+                            exp: Expr::Ident("result".to_string()),
+                            ty: Type::Int,
+                        }),
+                        value: Box::new(call_expr(
+                            ExprD {
+                                exp: Expr::Ident("double".to_string()),
+                                ty: double_ty.clone(),
+                            },
+                            vec![int_lit(21)],
+                            Type::Int,
+                        )),
                     },
                     ty: Type::Unit,
                 },
@@ -134,15 +177,69 @@ fn test_lambda_assignment_and_indirect_call() {
     let mut env = Environment::new();
     let instructions = translate_statement(stmt, &mut env);
 
-    assert_eq!(instructions, vec![
-        Instruction::JMP("Label1:".to_string()),
-        Instruction::Label("lambda_1".to_string()),
-        Instruction::BinaryAssignment(Operator::Mul, Address::Temporary("temp1".to_string(), Type::Int), Address::Variable("x".to_string(), Type::Int), Address::Constant(Literal::Int(2), Type::Int)),
-        Instruction::Return(Some(Address::Temporary("temp1".to_string(), Type::Int))),
-        Instruction::Label("Label1:".to_string()),
-        Instruction::CopyAssignment(Address::Variable("double".to_string(), double_ty.clone()), Address::FunctionLabel("lambda_1".to_string())),
-        Instruction::Param(Address::Constant(Literal::Int(21), Type::Int)),
-        Instruction::CallIndirect(Some(Address::Temporary("temp2".to_string(), Type::Int)), Address::Variable("double".to_string(), double_ty), 1),
-        Instruction::CopyAssignment(Address::Variable("result".to_string(), Type::Int), Address::Temporary("temp2".to_string(), Type::Int)),
-    ]);
+    assert_eq!(
+        instructions,
+        vec![
+            Instruction::JMP("Label1:".to_string()),
+            Instruction::Label("lambda_0".to_string()),
+            Instruction::BinaryAssignment(
+                Operator::Mul,
+                Address::Temporary("temp1".to_string(), Type::Int),
+                Address::Variable("x".to_string(), Type::Int),
+                Address::Constant(Literal::Int(2), Type::Int)
+            ),
+            Instruction::Return(Some(Address::Temporary("temp1".to_string(), Type::Int))),
+            Instruction::Label("Label1:".to_string()),
+            Instruction::CopyAssignment(
+                Address::Variable("double".to_string(), double_ty.clone()),
+                Address::FunctionLabel("lambda_0".to_string())
+            ),
+            Instruction::Param(Address::Constant(Literal::Int(21), Type::Int)),
+            Instruction::CallIndirect(
+                Some(Address::Temporary("temp2".to_string(), Type::Int)),
+                Address::Variable("double".to_string(), double_ty),
+                1
+            ),
+            Instruction::CopyAssignment(
+                Address::Variable("result".to_string(), Type::Int),
+                Address::Temporary("temp2".to_string(), Type::Int)
+            ),
+        ]
+    );
+}
+
+fn test_lambda_declaration_generates_function_label_and_body() {
+    let double_ty = fn_int_int();
+
+    let stmt = StatementD {
+        stmt: Statement::Decl {
+            name: "double".to_string(),
+            ty: double_ty.clone(),
+            init: Some(Box::new(lambda_double())),
+        },
+        ty: Type::Unit,
+    };
+
+    let mut env = Environment::new();
+    let instructions = translate_statement(stmt, &mut env);
+
+    assert_eq!(
+        instructions,
+        vec![
+            Instruction::JMP("Label1:".to_string()),
+            Instruction::Label("lambda_0".to_string()),
+            Instruction::BinaryAssignment(
+                Operator::Mul,
+                Address::Temporary("temp1".to_string(), Type::Int),
+                Address::Variable("x".to_string(), Type::Int),
+                Address::Constant(Literal::Int(2), Type::Int),
+            ),
+            Instruction::Return(Some(Address::Temporary("temp1".to_string(), Type::Int))),
+            Instruction::Label("Label1:".to_string()),
+            Instruction::CopyAssignment(
+                Address::Variable("double".to_string(), double_ty),
+                Address::FunctionLabel("lambda_0".to_string()),
+            ),
+        ]
+    );
 }
